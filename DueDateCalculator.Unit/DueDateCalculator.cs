f6bc77d14cc6401059dfc74300756d4f4939fdb0
Shem <42591036+ShemJM@users.noreply.github.com>
2021-09-25 02:10:38 +0100

    Add project files.

---
+++ DueDateCalculator.Unit/DueDateCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DueDateCalculator.Unit
+{
+    class DueDateCalculator
+    {
+        private readonly DayOfWeek[] _workDays = new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
+
+        private int _workDaysCount => _workDays.Length;
+
+        private readonly int _workHoursAmount = 8;
+
+        private readonly double _startTime = 9.00;
+
+        private readonly double _endTime = 17.00;
+
+        public DueDateCalculator()
+        {
+
+        }
+
+        public DateTime CalculateDueDate(DateTime submitDate, TimeSpan turnaroundTime)
+        {
+            var hoursRemainingInSubmittedDate = _endTime - submitDate.TimeOfDay.TotalHours;
+
+            var remainingTurnaroundTime = turnaroundTime.TotalHours - hoursRemainingInSubmittedDate;
+
+            var excessHours = remainingTurnaroundTime % _workHoursAmount;
+
+            var endTime = excessHours > 0 ? _startTime + excessHours : _endTime;
+
+            endTime = excessHours < 0 ? _endTime + excessHours : endTime;
+
+            var turnaroundTimeWorkDays = remainingTurnaroundTime / _workHoursAmount;
+
+            var dueDateDayOfTheWeek = GetNextWorkingDay(submitDate, (int)Math.Round(turnaroundTimeWorkDays + 0.49));
+
+            var dueDate = new DateTime(dueDateDayOfTheWeek.Year, dueDateDayOfTheWeek.Month, dueDateDayOfTheWeek.Day).AddHours(endTime);
+
+            return dueDate;
+        }
+
+        private DateTime GetNextWorkingDay(DateTime date, int days)
+        {
+            var weeks = days > 7 ? Math.Round(days / 7.00) : 0;
+            var dayIndex = (int)date.DayOfWeek + days;
+
+            while (dayIndex > _workDaysCount)
+            {
+                dayIndex -= _workDaysCount;
+            }
+
+            while (date.DayOfWeek != _workDays[dayIndex - 1])
+            {
+                date = date.AddDays(1);
+            }
+
+            return date.AddDays(weeks * 7);
+        }
+    }
+}
---
+++ DueDateCalculator.Unit/DueDateCalculatorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DueDateCalculator.Unit
+{
+    public class DueDateCalculatorTests
+    {
+        [Fact]
+        public void DueDate_ShouldBeCorrect()
+        {
+            var calculator = new DueDateCalculator();
+
+            foreach (var testCase in TestCases)
+            {
+                var dueDate = calculator.CalculateDueDate(testCase.SubmitDate, testCase.TurnaroundTime);
+
+                Assert.Equal(testCase.ExpectedDueDate, dueDate);
+            }
+        }
+
+        private List<DueDateCalculatorTestCase> TestCases => new()
+        {
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 21, 14, 12, 0), new TimeSpan(16, 00, 0), new DateTime(2021, 9, 23, 14, 12, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 21, 16, 59, 0), new TimeSpan(8, 0, 0), new DateTime(2021, 9, 22, 16, 59, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 21, 16, 59, 0), new TimeSpan(16, 0, 0), new DateTime(2021, 9, 23, 16, 59, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 24, 12, 00, 0), new TimeSpan(12, 00, 0), new DateTime(2021, 9, 27, 16, 0, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 12, 00, 0), new TimeSpan(8, 30, 0), new DateTime(2021, 9, 28, 12, 30, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(120, 00, 0), new DateTime(2021, 10, 15, 17, 0, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(8, 00, 0), new DateTime(2021, 9, 27, 17, 0, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(40, 00, 0), new DateTime(2021, 10, 1, 17, 0, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(160, 00, 0), new DateTime(2021, 10, 22, 17, 0, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(2, 00, 30), new DateTime(2021, 9, 27, 11, 00, 30)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(2, 00, 0), new DateTime(2021, 9, 27, 11, 00, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(1, 45, 0), new DateTime(2021, 9, 27, 10, 45, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(1, 00, 0), new DateTime(2021, 9, 27, 10, 00, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 9, 27, 9, 00, 0), new TimeSpan(0, 45, 0), new DateTime(2021, 9, 27, 9, 45, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 12, 31, 9, 00, 0), new TimeSpan(15, 00, 0), new DateTime(2022, 01, 03, 16, 00, 0)),
+            new DueDateCalculatorTestCase(new DateTime(2021, 12, 24, 9, 00, 0), new TimeSpan(40, 00, 0), new DateTime(2021, 12, 30, 17, 00, 0))
+        };
+
+        public class DueDateCalculatorTestCase
+        {
+            public DueDateCalculatorTestCase(DateTime submitDate, TimeSpan turnaroundTime, DateTime expectedDueDate)
+            {
+                SubmitDate = submitDate;
+                TurnaroundTime = turnaroundTime;
+                ExpectedDueDate = expectedDueDate;
+            }
+
+            public DateTime SubmitDate { get; set; }
+            public TimeSpan TurnaroundTime { get; set; }
+            public DateTime ExpectedDueDate { get; set; }
+        }
+    }
+}
